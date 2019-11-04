38b5ab0f60168a1c53671660c5dbf1323564b24c
David <lin.david985@gmail.com>
2019-11-03 23:41:18 -0500

    added x and o. Working on game logic

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var gameButtons: [GameButton]!
+    var xPlayer = false // false = "x"
     
+    var buttonPressed = [0, 0, 0, 0, 0, 0, 0, 0, 0] // keep track of buttons pressed
+    
+    @IBOutlet var gameButtons: [GameButton]!
+    @IBOutlet weak var turnLabel: UILabel!
     
     
     
@@ -19,12 +23,35 @@ class ViewController: UIViewController {
     
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    turnLabel.text = "Player One Turn (❌)"
   }
 
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
-        print("row \(gameButton.row) at column \(gameButton.col) was selected")
+        
+        if (buttonPressed[gameButton.tag] == 0) {  // tags start at 1 and index starts at 0
+        
+        buttonPressed[gameButton.tag] = 1 // sets button = 1 so you can no longer press it after use
+            
+        xPlayer.toggle()
+        if xPlayer {
+            gameButton.setImage(UIImage(named: "xdonut"), for: UIControl.State())
+            turnLabel.text = "PLAYER TWO'S TURN (⭕️)"
+        } else {
+            gameButton.setImage(UIImage(named: "odonut"), for: UIControl.State())
+            turnLabel.text = "PLAYER ONE'S TURN (❌)"
+        }
+        }
     }
+    
+    
+    
+    @IBAction func startButton(_ sender: UIButton) {
+       turnLabel.text = "Player One Turn (❌)"
+        
+    }
+    
+    
+    
     
 }
 
