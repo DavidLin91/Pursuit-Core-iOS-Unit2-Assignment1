84614b538a5dfa23d07ec4be48a319fdeaa04434
David <lin.david985@gmail.com>
2019-11-06 00:01:41 -0500

    finished tictactoe

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -13,11 +13,12 @@ class ViewController: UIViewController {
     var xPlayerStatus = TicTacToeBrain.init().xPlayer
     var winComboLogic = TicTacToeBrain.init().winCombo // all possible win combos
     var buttonPressed = [0, 0, 0, 0, 0, 0, 0, 0, 0] // keep track of buttons pressed
+    var winningRowX = [Int]()
+    var winningRowO = [Int]()
     @IBOutlet var gameButtons: [GameButton]!
     @IBOutlet weak var turnLabel: UILabel!
     var playingGame = true  // playing, no winner
-    var winningRowX = [Int]()
-    var winningRowO = [Int]()
+    
     
     
   override func viewDidLoad() {
@@ -28,16 +29,17 @@ class ViewController: UIViewController {
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
         
         if (buttonPressed[gameButton.tag] == 0) {  // tags start at 1 and index starts at 0
-        
         buttonPressed[gameButton.tag] = 1 // sets button = 1 so you can no longer press it after use
-            
+
         xPlayerStatus.toggle() // true
         if xPlayerStatus {
             gameButton.setImage(UIImage(named: "xdonut"), for: UIControl.State())
             turnLabel.text = "PLAYER TWO'S TURN (⭕️)"
             winningRowX.append(gameButton.tag)
+            let setRowX = Set(winningRowX) // changed array to set to use .intersect
             for arr in winComboLogic {
-                if winningRowX.sorted() == arr {
+                print(Array(setRowX.intersection(Set(arr))))
+                if Array(setRowX.intersection(Set(arr))).sorted() == arr { // changed set back to array and sorted to compare to array
                   turnLabel.text = "PLAYER ONE WINS!"
                   buttonPressed = [1, 1, 1, 1, 1, 1, 1, 1, 1]
                 }
@@ -47,8 +49,9 @@ class ViewController: UIViewController {
             gameButton.setImage(UIImage(named: "odonut"), for: UIControl.State())
             turnLabel.text = "PLAYER ONE'S TURN (❌)"
              winningRowO.append(gameButton.tag)
+            let setRowO = Set(winningRowO)
             for arr in winComboLogic {
-                if winningRowO.sorted() == arr {
+                if Array(setRowO.intersection(Set(arr))).sorted() == arr {
                     turnLabel.text = "PLAYER TWO WINS!"
                     buttonPressed = [1, 1, 1, 1, 1, 1, 1, 1, 1]
                 }
